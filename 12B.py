f7b81cb85df0de441a90e9fe25c45f129ff37684
V Lokesh <lokeshvishwanath10@gmail.com>
2026-05-01 15:54:20 +0530

    day 12B practice

---
+++ 12B.py
@@ -0,0 +1,5 @@
+programming_languages_list = ["Python", "Java", "Ruby", "C", "C++", "Go", "R", "JavaScript", "Swift", "PHP", "Kotlin", "Perl"]
+m = int(input()) 
+for i in range(m):
+    index = int(input()) 
+    print(programming_languages_list[index])
