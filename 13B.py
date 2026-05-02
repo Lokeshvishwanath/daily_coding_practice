a9fa54d359b15a37afb349c293aa60198b321450
V Lokesh <lokeshvishwanath10@gmail.com>
2026-05-02 23:57:24 +0530

    daily 13B coding practice

---
+++ 13B.py
@@ -0,0 +1,18 @@
+nums = "1 2 3 4"
+num_list = nums.split()
+print(num_list)
+
+
+string_a = "step-by-step execution of code"
+list_a = string_a.split('step')
+print(list_a)
+
+
+list_a = ['Python is ', ' progr', 'mming l', 'ngu', 'ge']
+string_a = "a".join(list_a)
+print(string_a)
+
+
+list_a = [5, 4, 3, 2, 1]
+list_b = list_a[::-1]
+print(list_b)
