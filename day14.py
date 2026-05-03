55e7d13d31e243b3319d821f313639ecccda1ca5
V Lokesh <lokeshvishwanath10@gmail.com>
2026-05-03 23:25:51 +0530

    day 14 coding pracice

---
+++ day14.py
@@ -0,0 +1,12 @@
+list_a = [5, 4, 3, 2, 1] // Negative Indexing
+list_b = list_a[::-1]
+print(list_b) 
+
+list_a = [5, 4, 3, 2, 1]
+list_b = list_a[-3:-1]
+print(list_b)
+
+list_a = [5, 4, 3, 2, 1]
+list_b = list_a[-6:-2]
+print(list_b)
+
