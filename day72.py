c40cbfeec2fc898ba342d43b95a93804b15ce670
V Lokesh <lokeshvishwanath10@gmail.com>
2026-07-11 23:53:45 +0530

    day72 coding practice

--- day72.py
+++ day72.py
@@ -30,3 +30,43 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# day13_leader_elements.py
+
+class Solution:
+
+    def find_leaders(self, arr):
+
+        leaders = []
+
+        max_from_right = arr[-1]
+
+        leaders.append(max_from_right)
+
+        for i in range(len(arr) - 2, -1, -1):
+
+            if arr[i] > max_from_right:
+
+                leaders.append(arr[i])
+                max_from_right = arr[i]
+
+        leaders.reverse()
+
+        return leaders
+
+
+def main():
+
+    arr = [16, 17, 4, 3, 5, 2]
+
+    solution = Solution()
+
+    result = solution.find_leaders(arr)
+
+    print("Array:", arr)
+    print("Leader Elements:", result)
+
+
+if __name__ == "__main__":
+    main()
