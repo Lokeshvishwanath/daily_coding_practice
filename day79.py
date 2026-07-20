5d76bb04a9a3c2265d8748efbd0333c38de31964
V Lokesh <lokeshvishwanath10@gmail.com>
2026-07-20 23:33:53 +0530

    day79 coding practice

--- day79.py
+++ day79.py
@@ -30,3 +30,41 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# day19A_maximum_product_subarray.py
+
+class Solution:
+
+    def max_product(self, nums):
+
+        max_product = nums[0]
+        min_product = nums[0]
+        answer = nums[0]
+
+        for i in range(1, len(nums)):
+
+            if nums[i] < 0:
+                max_product, min_product = min_product, max_product
+
+            max_product = max(nums[i], max_product * nums[i])
+            min_product = min(nums[i], min_product * nums[i])
+
+            answer = max(answer, max_product)
+
+        return answer
+
+
+def main():
+
+    nums = [2, 3, -2, 4]
+
+    solution = Solution()
+
+    result = solution.max_product(nums)
+
+    print("Array :", nums)
+    print("Maximum Product:", result)
+
+
+if __name__ == "__main__":
+    main()
