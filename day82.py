f1047d9c387dced1a1d489e0f6be7bf64f149d21
V Lokesh <lokeshvishwanath10@gmail.com>
2026-07-24 23:59:56 +0530

    day81 coding practice

--- day82.py
+++ day82.py
@@ -32,3 +32,45 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# day22A_kth_largest_element.py
+
+import heapq
+
+
+class Solution:
+
+    def find_kth_largest(self, nums, k):
+
+        min_heap = nums[:k]
+
+        heapq.heapify(min_heap)
+
+        for num in nums[k:]:
+
+            if num > min_heap[0]:
+
+                heapq.heappop(min_heap)
+
+                heapq.heappush(min_heap, num)
+
+        return min_heap[0]
+
+
+def main():
+
+    nums = [3, 2, 1, 5, 6, 4]
+    k = 2
+
+    solution = Solution()
+
+    result = solution.find_kth_largest(nums, k)
+
+    print("Array :", nums)
+    print("k =", k)
+    print("Kth Largest Element:", result)
+
+
+if __name__ == "__main__":
+    main()
