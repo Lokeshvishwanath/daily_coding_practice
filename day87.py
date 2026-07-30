70af225d1de6d2faa494eede891cffc4bfc9b4a3
V Lokesh <lokeshvishwanath10@gmail.com>
2026-07-30 23:11:05 +0530

    day87 coding practice

--- day87.py
+++ day87.py
@@ -70,3 +70,19 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the number";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=n;j>0;j--){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+}
