0ee74c9f3a455ed992abff47166305d2da378b0f
V Lokesh <lokeshvishwanath10@gmail.com>
2026-08-01 14:56:34 +0530

    day88 coding practice

--- day88.py
+++ day88.py
@@ -6,3 +6,14 @@ def pattern1():
         for j in range(n-i):
             print("*", end=" ") 
         print()
+
+def pattern2():
+    n = 4
+    for i in range(4):
+        for j in range(n-i-1):
+            print(" ", end=" ") 
+        for j in range(2*i+1):
+            print("*", end=" ") 
+        print() 
+
+pattern2()
