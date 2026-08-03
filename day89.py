d92af2894a258045fb940076e5333145f8aac997
V Lokesh <lokeshvishwanath10@gmail.com>
2026-08-03 22:59:55 +0530

    day89 coding practice

---
+++ day89.py
@@ -0,0 +1,41 @@
+# day27_search_insert_position.py
+
+class Solution:
+
+    def search_insert(self, nums, target):
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            elif nums[mid] < target:
+                left = mid + 1
+
+            else:
+                right = mid - 1
+
+        return left
+
+
+def main():
+
+    nums = [1, 3, 5, 6]
+    target = 2
+
+    solution = Solution()
+
+    result = solution.search_insert(nums, target)
+
+    print("Array :", nums)
+    print("Target:", target)
+    print("Insert Position:", result)
+
+
+if __name__ == "__main__":
+    main()
