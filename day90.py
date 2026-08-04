de9c4cf360e5f533f63806791c4ad786ea7ae715
V Lokesh <lokeshvishwanath10@gmail.com>
2026-08-04 23:20:03 +0530

    day90 coding practice

--- day90.py
+++ day90.py
@@ -6,3 +6,16 @@ def pattern1():
         for j in range(n-i):
             print("*", end=" ") 
         print()
+
+def hollow_rectangle():
+    rows = 4 
+    cols = 7 
+    for i in range(rows):
+        for j in range(cols):
+            if i == 0 or i == rows - 1 or j == 0 or j == cols - 1:
+                print("*",end=" ") 
+            else:
+                print(" ",end=" ") 
+        print()
+        
+hollow_rectangle()
