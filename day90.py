917da5fe03cbd817a7a1030ac3e73b98a2130885
V Lokesh <lokeshvishwanath10@gmail.com>
2026-08-04 23:17:57 +0530

    day90 coding practice

---
+++ day90.py
@@ -0,0 +1,8 @@
+def pattern1():
+    n = 4 
+    for i in range(n):
+        for j in range(i):
+            print(" ", end=" ")
+        for j in range(n-i):
+            print("*", end=" ") 
+        print()
