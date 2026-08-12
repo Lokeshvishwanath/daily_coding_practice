36f48fe263c4568b916a51b9f0f517cf2cab8257
V Lokesh <lokeshvishwanath10@gmail.com>
2026-08-12 23:59:00 +0530

    day95 coding practice

--- day95.py
+++ day95.py
@@ -34,3 +34,42 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# day31A_daily_temperatures.py
+
+class Solution:
+
+    def daily_temperatures(self, temperatures):
+
+        result = [0] * len(temperatures)
+
+        stack = []
+
+        for i in range(len(temperatures)):
+
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+
+                previous_day = stack.pop()
+
+                result[previous_day] = i - previous_day
+
+            stack.append(i)
+
+        return result
+
+
+def main():
+
+    temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
+
+    solution = Solution()
+
+    result = solution.daily_temperatures(temperatures)
+
+    print("Temperatures:", temperatures)
+    print("Days to Wait:", result)
+
+
+if __name__ == "__main__":
+    main()
