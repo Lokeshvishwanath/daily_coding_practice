49fcda8f833fcb81c8b9267cdfd7b78386676c2f
V Lokesh <lokeshvishwanath10@gmail.com>
2026-08-14 23:56:19 +0530

    day97 coding practice

--- day97.py
+++ day97.py
@@ -48,3 +48,45 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# day32_stock_span.py
+
+class Solution:
+
+    def calculate_span(self, prices):
+
+        span = [0] * len(prices)
+
+        stack = []
+
+        for i in range(len(prices)):
+
+            while stack and prices[stack[-1]] <= prices[i]:
+                stack.pop()
+
+            if not stack:
+                span[i] = i + 1
+
+            else:
+                span[i] = i - stack[-1]
+
+            stack.append(i)
+
+        return span
+
+
+def main():
+
+    prices = [100, 80, 60, 70, 60, 75, 85]
+
+    solution = Solution()
+
+    result = solution.calculate_span(prices)
+
+    print("Stock Prices:", prices)
+    print("Stock Span:", result)
+
+
+if __name__ == "__main__":
+    main()
